08ec71e30639e0ad549505a3eebad37fc468496b
Janosch Gräf <janosch.graef@gmail.com>
2026-04-28 20:36:25 +0200

    wip: going to switch to dioxus

--- stolas-webui/src/app/mod.rs
+++ stolas-webui/src/app/mod.rs
@@ -1,5 +1,6 @@
 pub mod observe;
 pub mod status;
+pub mod util;
 
 use futures_util::TryStreamExt;
 use leptos::{
@@ -13,25 +14,7 @@ use leptos_meta::{
     Link,
     provide_meta_context,
 };
-use leptos_router::{
-    components::{
-        A,
-        Route,
-        Router,
-        Routes,
-    },
-    path,
-};
-use leptos_styling::{
-    StyleSheets,
-    style_sheet,
-};
 use url::Url;
-
-use crate::app::{
-    observe::ObservePage,
-    status::StatusPage,
-};
 
 fn get_base_url() -> Option<Url> {
     gloo_utils::document().base_uri().ok()??.parse().ok()
@@ -43,8 +26,6 @@ pub fn default_api_url() -> Url {
     tracing::debug!(%api_url);
     api_url
 }
-
-style_sheet!(app, "src/app/app.scss", "app");
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -58,61 +39,7 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Link rel="icon" type_="image/x-icon" href="/static/favicon.png" />
-        <StyleSheets />
-        <Router base="/ui">
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Navbar">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">"Stolas"</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#top_navbar" aria-controls="top_navbar" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="top_navbar">
-                        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">"Dashboard"</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">"Link"</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">"Disabled"</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item" href="#">Action</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form role="search">
-                            <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-                        </form>
-                    </div>
-                </div>
-            </nav>
-            /*<div>
-                <nav>
-                    <div>
-                         <A href="/">"Status"</A>
-                         <A href="/observe">"Observe"</A>
-                    </div>
-                </nav>
-                <main>
-                    <Routes fallback=PageNotFound>
-                        <Route path=path!("/") view=StatusPage />
-                        <Route path=path!("/observe") view=ObservePage />
-                    </Routes>
-                </main>
-            </div>*/
-        </Router>
+        <h1>"Hello World"</h1>
     }
 }
 
